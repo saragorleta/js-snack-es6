7206535d0235eda10ffbee55b636e545a600b766
Sara <saragorleta@yahoo.com>
2021-01-19 13:10:35 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+1) Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero,
+ se è dispari stampa il numero successivo.
+2) Utilizzando const e/o let e template literal.
+Creare due tag div con due id diversi.
+Un div avrà il testo colorato di rosso mentre l’altro di verde.
+Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e
+in verde i numeri pari.
+3) Utilizzando const e/o let. Dare la possibilità di inserire due parole.
+Verificare se le due parole hanno la stessa lunghezza.
+Se hanno la stessa lunghezza, stamparle entrambe altrimenti
+stampare la più lunga delle due.
+
+
+
+1)
+const numero=[];
